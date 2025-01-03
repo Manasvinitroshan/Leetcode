f2be8646057933a6049efda7227b6d0a2ab122f2
Manasvinitroshan <manas.comet.25@gmail.com>
2025-01-03 16:52:34 -0600

    lengthofLastWord added

--- 150 Questions/lengthOfLastWord.py
+++ 150 Questions/lengthOfLastWord.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
 
-        text = s.strip().split()
+        text = s.strip().split() # strip comes before split
         
         return len(text[-1])
